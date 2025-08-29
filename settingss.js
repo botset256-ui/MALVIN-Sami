64e809342a36b470efce892d9c556a3f32fb737f
botset256-ui <botset256@gmail.com>
2025-08-29 19:33:32 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~VnFV1IJT#leMu3btwGmgLvO40PJkAzGyfJQN4ziQmSZ2GTxGcZ8A",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
